a155625e5b0a26ea67b08191965f4cd1f4691ac2
Valentin I. Burlacu <blxyon@yahoo.com>
2022-04-06 20:53:50 +0100

    removed comments

--- test/unitesting.py
+++ test/unitesting.py
@@ -35,46 +35,6 @@ class TestSudoku(unittest.TestCase):
             sudoku_solved=sor.sudoku_solver(sudoku)
             np.testing.assert_array_equal(solutions[i], sudoku_solved)
 
-    # def test_divide(self):
-    #     self.assertEqual(calc.divide(10, 5), 2)
-    #     self.assertEqual(calc.divide(-1, 1), -1)
-    #     self.assertEqual(calc.divide(-1, -1), 1)
-    #     self.assertEqual(calc.divide(5, 2), 2.5)
-
-    #     with self.assertRaises(ValueError):
-    #         calc.divide(10, 0)
-
 
 if __name__ == '__main__':
     unittest.main()
-# # Load sudokus
-# sudokus = np.load("data/sudokus.npy")
-# #print("Shape of one sudoku array:", sudokus[0].shape, ". Type of array values:", sudokus.dtype)
-
-# # Load solutions
-# solutions = np.load("data/sudoku_solutions.npy")
-
-# r=range(0,20)
-# for i in r:
-# 	sudoku = sudokus[i].copy()
-# 	print("This is sudoku number", i)
-# 	print(sudoku)
-# 	print("This is solution for s number", i)
-# 	print(solutions[i])
-# 	your_solution=swor.sudoku_solver(sudoku)
-# 	print("This is your solution for sudoku number", i)
-# 	print(your_solution)
-# 	print("Is your solution correct?")
-# 	print(np.array_equal(your_solution, solutions[i]))
-
-# for i in r:
-# 	sudoku = sudokus[i].copy()
-# 	print("This is sudoku number", i)
-# 	print(sudoku)
-# 	print("This is solution for s number", i)
-# 	print(solutions[i])
-# 	your_solution=sor.solveSudoku(sudoku)
-# 	print("This is your solution for sudoku number", i)
-# 	print(your_solution)
-# 	print("Is your solution correct?")
-# 	print(np.array_equal(your_solution, solutions[i]))
